2004407fbe01988be5e689aef20c182462d56a63
nikitakosatka <nikitakosatka@yandex.ru>
2019-04-03 22:09:29 +0500

    Поправил задачу №2

--- okaymaps/main_window.py
+++ okaymaps/main_window.py
@@ -10,15 +10,13 @@ class MainWindow(QMainWindow):
         uic.loadUi(WINDOW_PATH, self)
         self.map_widget = Map(self.map)
 
-    def keyPressEvent(self, event):
+    def keyReleaseEvent(self, event):
         key = event.key()
 
         if key == QtCore.Qt.Key_PageUp and self.map_widget.z < 19:
-            print(1)
             self.map_widget.z += 1
             self.map_widget.set_image(get_image(self.map_widget))
 
         if key == QtCore.Qt.Key_PageDown and self.map_widget.z > 0:
-            print(2)
             self.map_widget.z -= 1
             self.map_widget.set_image(get_image(self.map_widget))
